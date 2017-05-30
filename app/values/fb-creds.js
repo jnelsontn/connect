fd12989c46e223d939172cdb435f58b349f51cfd
Jordan Nelson <jnelsontn@gmail.com>
2017-05-30 17:34:11 -0500

    FB credentials need to be here for GitHub hosting

--- app/values/fb-creds.js
+++ app/values/fb-creds.js
@@ -1,8 +1,8 @@
 'use strict';
 
 app.constant('FBCreds', {
-	apiKey: '',
-	authDomain: '',
-	databaseURL: '',
-	storageBucket: ''
+	apiKey: 'AIzaSyCwryqIUxOwej6zlK9WertIZEGgobOW9Qw',
+	authDomain: 'couplesconnect-b48b7.firebaseapp.com',
+	databaseURL: 'https://couplesconnect-b48b7.firebaseio.com',
+	storageBucket: 'couplesconnect-b48b7.appspot.com'
 });
