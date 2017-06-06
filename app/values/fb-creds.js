25e7640c3417843c4d0f710710f6c84b10721901
Jordan Nelson <jnelsontn@gmail.com>
2017-06-06 10:52:14 -0500

    updated for domain change to connect.digitalmarshmellow.com

--- app/values/fb-creds.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-app.constant('FBCreds', {
-	apiKey: 'AIzaSyCwryqIUxOwej6zlK9WertIZEGgobOW9Qw',
-	authDomain: 'couplesconnect-b48b7.firebaseapp.com',
-	databaseURL: 'https://couplesconnect-b48b7.firebaseio.com',
-	storageBucket: 'couplesconnect-b48b7.appspot.com'
-});
