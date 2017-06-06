f8d9a9da7eb4092edca248ac4c45e59ef7edd15c
Jordan Nelson <jnelsontn@gmail.com>
2017-06-06 14:12:34 -0500

    fixed error with notifications

--- app/controllers/NotificationsCtrl.js
+++ app/controllers/NotificationsCtrl.js
@@ -1,7 +1,12 @@
 'use strict';
 
-app.controller('NotificationsCtrl', function($scope, $firebaseArray, AuthFactory) {
+app.controller('NotificationsCtrl', function($scope, $routeParams, $firebaseArray, AuthFactory) {
 	let userLoggedIn = AuthFactory.getUser();
+	let userUID = $routeParams.profileId;
 	let notificationsRef = firebase.database().ref('notifications/' + userLoggedIn);
-	$scope.notifications = $firebaseArray(notificationsRef);
+
+	if (userLoggedIn === userUID) {
+		$scope.myOwnProfile = true;
+		$scope.notifications = $firebaseArray(notificationsRef);
+	}
 });
